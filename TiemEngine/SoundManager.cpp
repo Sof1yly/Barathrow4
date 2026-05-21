32b8315fb5efcf353401ba1996894f588e973904
Pupa Tha <pupa.tha12@gmail.com>
2026-05-21 19:07:02 +0700

    Update sound assets and SFX paths
    Add new MP3 assets (BGM/BGM.mp3, OnLose.mp3, Player_Hurt.mp3, UI_Click.mp3) and update SoundManager to use organized ../Resource/Sound/... .mp3 paths for enemy, elite, boss, player and UI SFX, replacing previous placeholder .wav entries. Also update BGM_FILE in SoundManager.h to point to the new BGM.mp3.

--- TiemEngine/SoundManager.cpp
+++ TiemEngine/SoundManager.cpp
@@ -8,54 +8,54 @@
 static const char* FILE_PATHS[] =
 {
     // ---- Regular enemy attacks ----
-    /* ENEMY_A_ATTACK  */ "../Resource/Sound/medium.wav",
-    /* ENEMY_B_ATTACK  */ "../Resource/Sound/medium.wav",
-    /* ENEMY_C_ATTACK  */ "../Resource/Sound/medium.wav",
-    /* ENEMY_D_ATTACK  */ "../Resource/Sound/medium.wav",
-    /* ENEMY_E_ATTACK  */ "../Resource/Sound/medium.wav",
-    /* ENEMY_F_ATTACK  */ "../Resource/Sound/medium.wav",
-    /* ENEMY_G_ATTACK  */ "../Resource/Sound/medium.wav",
-    /* ENEMY_H_ATTACK  */ "../Resource/Sound/medium.wav",
+    /* ENEMY_A_ATTACK  */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Type_A/NormalA_Attack.mp3",
+    /* ENEMY_B_ATTACK  */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Type_B/NormalB_Attack.mp3",
+    /* ENEMY_C_ATTACK  */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Type_C/NormalC_Attack.mp3",
+    /* ENEMY_D_ATTACK  */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Type_D/EliteA_Attack.mp3",
+    /* ENEMY_E_ATTACK  */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Type_E/NormalE_Attack.mp3",
+    /* ENEMY_F_ATTACK  */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Type_F/NormalF_Attack.mp3",
+    /* ENEMY_G_ATTACK  */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Type_G/NormalG_Attack.mp3",
+    /* ENEMY_H_ATTACK  */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Type_H/NormalG_Attack.mp3",
 
     // ---- Regular enemy moves ----
-    /* ENEMY_A_MOVE    */ "../Resource/Sound/low.wav",
-    /* ENEMY_B_MOVE    */ "../Resource/Sound/low.wav",
-    /* ENEMY_C_MOVE    */ "../Resource/Sound/low.wav",
-    /* ENEMY_D_MOVE    */ "../Resource/Sound/low.wav",
-    /* ENEMY_E_MOVE    */ "../Resource/Sound/low.wav",
-    /* ENEMY_F_MOVE    */ "../Resource/Sound/low.wav",
-    /* ENEMY_G_MOVE    */ "../Resource/Sound/low.wav",
-    /* ENEMY_H_MOVE    */ "../Resource/Sound/low.wav",
+    /* ENEMY_A_MOVE    */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Type_A/NormalA_Move.mp3",
+    /* ENEMY_B_MOVE    */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Type_B/RobotMove.mp3",
+    /* ENEMY_C_MOVE    */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Type_C/RobotMove.mp3",
+    /* ENEMY_D_MOVE    */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Type_D/RobotMove.mp3",
+    /* ENEMY_E_MOVE    */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Type_E/NormalA_Move.mp3",
+    /* ENEMY_F_MOVE    */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Type_F/NormalA_Move.mp3",
+    /* ENEMY_G_MOVE    */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Type_G/RobotMove.mp3",
+    /* ENEMY_H_MOVE    */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Type_H/NormalA_Move.mp3",
 
     // ---- General enemy events ----
-    /* ENEMY_TAKE_DAMAGE */ "../Resource/Sound/scratch.wav",
-    /* ENEMY_DIES        */ "../Resource/Sound/high.wav",
+    /* ENEMY_TAKE_DAMAGE */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Enemy_TakeDmg.mp3",
+    /* ENEMY_DIES        */ "../Resource/Sound/Enemy_Sound/Normal_Enemy/Monster_Dies.mp3",
 
     // ---- Elite enemies ----
-    /* ELITE_A_ATTACK  */ "../Resource/Sound/medium.wav",
-    /* ELITE_B_ATTACK1 */ "../Resource/Sound/medium.wav",
-    /* ELITE_B_ATTACK2 */ "../Resource/Sound/scratch.wav",
-    /* ELITE_C_ATTACK1 */ "../Resource/Sound/medium.wav",
-    /* ELITE_C_ATTACK2 */ "../Resource/Sound/scratch.wav",
+    /* ELITE_A_ATTACK  */ "../Resource/Sound/Enemy_Sound/Elite_Enemy/Elite_A/EliteA_Attack.mp3",
+    /* ELITE_B_ATTACK1 */ "../Resource/Sound/Enemy_Sound/Elite_Enemy/Elite_B/Atk_1/NormalF_Attack.mp3",
+    /* ELITE_B_ATTACK2 */ "../Resource/Sound/Enemy_Sound/Elite_Enemy/Elite_B/Atk_2/EliteB_Attack2.mp3",
+    /* ELITE_C_ATTACK1 */ "../Resource/Sound/Enemy_Sound/Elite_Enemy/Elite_C/Atk_1/NormalG_Attack.mp3",
+    /* ELITE_C_ATTACK2 */ "../Resource/Sound/Enemy_Sound/Elite_Enemy/Elite_C/Atk_2/NormalF_Attack.mp3",
 
     // ---- Boss ----
-    /* BOSS_BOARD_WIDE */ "../Resource/Sound/medium.wav",
-    /* BOSS_SUMMON     */ "../Resource/Sound/scratch.wav",
-    /* BOSS_LASER      */ "../Resource/Sound/high.wav",
+    /* BOSS_BOARD_WIDE */ "../Resource/Sound/Enemy_Sound/Boss_Enemy/Boss_Stomping.mp3",
+    /* BOSS_SUMMON     */ "../Resource/Sound/Enemy_Sound/Boss_Enemy/Boss_Summon.mp3",
+    /* BOSS_LASER      */ "../Resource/Sound/Enemy_Sound/Boss_Enemy/NormalA_Attack.mp3",
 
     // ---- Player ----
-    /* PLAYER_ATTACK_MELEE      */ "../Resource/Sound/medium.wav",
-    /* PLAYER_ATTACK_MELEE_SPIN */ "../Resource/Sound/medium.wav",
-    /* PLAYER_ATTACK_RANGE      */ "../Resource/Sound/high.wav",
-    /* PLAYER_MOVE              */ "../Resource/Sound/low.wav",
-    /* PLAYER_TAKE_DAMAGE       */ "../Resource/Sound/scratch.wav",
-    /* PLAYER_DIES              */ "../Resource/Sound/scratch.wav",
+    /* PLAYER_ATTACK_MELEE      */ "../Resource/Sound/Player_Sound/Player_Attack/Player_Melee (1).mp3",
+    /* PLAYER_ATTACK_MELEE_SPIN */ "../Resource/Sound/Player_Sound/Player_Attack/Player_MeleeSpinNew.mp3",
+    /* PLAYER_ATTACK_RANGE      */ "../Resource/Sound/Player_Sound/Player_Attack/Player_Range.mp3",
+    /* PLAYER_MOVE              */ "../Resource/Sound/Player_Sound/Player_Move.mp3",
+    /* PLAYER_TAKE_DAMAGE       */ "../Resource/Sound/Player_Sound/Player_Hurt.mp3",
+    /* PLAYER_DIES              */ "../Resource/Sound/Player_Sound/Player_Dies.mp3",
 
     // ---- UI ----
-    /* UI_CLICK      */ "../Resource/Sound/low.wav",
+    /* UI_CLICK      */ "../Resource/Sound/UI_Sound/UI_Click.mp3",
     /* UI_HOVER_CARD */ "../Resource/Sound/low.wav",
-    /* UI_SHOP_BUY   */ "../Resource/Sound/high.wav",
-    /* UI_LOOT_COIN  */ "../Resource/Sound/high.wav",
+    /* UI_SHOP_BUY   */ "../Resource/Sound/UI_Sound/Shop_Buy.mp3",
+    /* UI_LOOT_COIN  */ "../Resource/Sound/UI_Sound/Collecting_Loot.mp3",
 };
 static_assert(sizeof(FILE_PATHS) / sizeof(FILE_PATHS[0]) == static_cast<int>(SoundManager::SFX::COUNT),
               "FILE_PATHS size must match SoundManager::SFX::COUNT");
